7d7792e8eaa98b1db95ed85784330abf70503322
Tao Su <tao@sengledcanada.com>
2017-05-19 21:07:47 -0700

    Make authentication works

--- launchpad/launchpad/api/debug.py
+++ launchpad/launchpad/api/debug.py
@@ -1,7 +1,10 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
-class Ping(APIView):
+class Ping(generics.GenericAPIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, format=None):
         return Response({'message': 'Pong'})
--- launchpad/launchpad/settings/default.py
+++ launchpad/launchpad/settings/default.py
@@ -132,6 +132,9 @@ AUTHENTICATION_BACKENDS = (
 REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
     )
 }
 
